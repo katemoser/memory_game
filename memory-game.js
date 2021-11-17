6ecae16da702834b01e840494d38fb6646c24a97
Kate Moser <komoser@gmail.com>
2021-11-17 11:00:53 +0000

    Added flip functionality and succcessfully detects a match

--- memory-game.js
+++ memory-game.js
@@ -9,6 +9,7 @@ const COLORS = [
 ];
 
 const colors = shuffle(COLORS);
+const selectedCards = [];
 
 createCards(colors);
 
@@ -43,8 +44,9 @@ function createCards(colors) {
 
   for (let color of colors) {
     let new_card = document.createElement("div");
-    new_card.className = color;
-    new_card.className = "back";
+    new_card.classList.add(color);
+    new_card.classList.add("back");
+    //new_card.className = "back";
     //add event listener
     new_card.addEventListener("click", handleCardClick);
     gameBoard.appendChild(new_card);
@@ -55,19 +57,54 @@ function createCards(colors) {
 /** Flip a card face-up. */
 
 function flipCard(card) {
-  card.className = "flipped";
+  card.classList.remove("back");
   // ... you need to write this ...
 }
 
 /** Flip a card face-down. */
 
 function unFlipCard(card) {
+  card.classList.add("back");
   // ... you need to write this ...
 }
 
 /** Handle clicking on a card: this could be first-card or second-card. */
 
 function handleCardClick(evt) {
-  alert("You have clicked on " + evt);
-  // ... you need to write this ...
+  let card = evt.target;
+  //alert("You have clicked on " + card);
+  flipCard(card);
+  //remove event listener so you can't click the same card twice
+  card.removeEventListener("click", handleCardClick);
+
+  //add this card to selected cards
+  selectedCards.push(card);
+
+  //check if we have a match if there are two cards in selectedCards array
+  if(selectedCards.length === 2){
+    checkForMatch();
+  }
+}
+
+/*  Check if the two cards in selectedCards array are the same color  */
+function checkForMatch(){
+  if(selectedCards[0].className === selectedCards[1].className){
+    handleMatch();
+  }
+  else{
+    handleNotMatch();
+  }
+}
+
+function handleMatch(){
+  alert("This is a match!");
+  //keep cards face up, and add 1 to score
+
+}
+
+function handleNotMatch(){
+  alert("Not a match!");
+
+  //flip cards back over, replace event listeners, and empty the selectedCards array
+
 }
