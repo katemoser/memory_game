942089eb63124df14c070c6f800d87cb169990fc
Kate Moser <komoser@gmail.com>
2021-12-30 08:41:08 -0500

    Added different sizes, fixed timing issue with clicks, added new game button

--- memory-game.js
+++ memory-game.js
@@ -4,18 +4,50 @@
 
 const FOUND_MATCH_WAIT_MSECS = 1000;
 const COLORS = [
-  "red", "blue", "green", "orange", "purple",
-  "red", "blue", "green", "orange", "purple",
+  "red", "blue", "green", "orange", "purple", "cyan", "white", "black",
 ];
-const NUM_PAIRS_TO_WIN = COLORS.length/2;
 
-const colors = shuffle(COLORS);
 const selectedCards = [];
 let score = 0;
+let clicks = 0;
+let numPairsToWin = 0;
 
-createCards(colors);
+
+//const colors = shuffle(COLORS);
+//createCards(colors);
+
+//add event listener to button
+document.getElementById("newGame").addEventListener("click", newGame);
 
 
+function newGame(){
+  //remove old cards, if any, resest scores etc
+  let board = document.getElementById("game");
+  while(board.firstChild){
+    board.removeChild(board.firstChild);
+  }
+  score = 0;
+  clicks = 0;
+  updateCounters();
+
+  //add new cards -- size from the display
+  let selectBox = document.getElementById("size");
+  let numPairs = parseInt(selectBox.options[selectBox.selectedIndex].value);
+  numPairsToWin = numPairs;
+  console.log(numPairsToWin);
+  const colors = shuffle(createDeck(numPairs));
+  createCards(colors);
+}
+
+/** create a new deck of cards of selected size */
+function createDeck(numPairs){
+  let deck = [];
+  for(let i = 0; i < numPairs; i++){
+    deck.push(COLORS[i]);
+    deck.push(COLORS[i]);
+  }
+  return deck;
+}
 /** Shuffle array items in-place and return shuffled array. */
 
 function shuffle(items) {
@@ -46,6 +78,7 @@ function createCards(colors) {
   for (let color of colors) {
     let new_card = document.createElement("div");
     new_card.classList.add(color);
+    new_card.classList.add("card");
     new_card.classList.add("back");  
     //add event listener
     new_card.addEventListener("click", handleCardClick);
@@ -58,10 +91,14 @@ function createCards(colors) {
 
 function flipCard(card) {
   card.classList.remove("back");
-  // ... you need to write this ...
+  //remove event listener so you can't click the same card twice
+  card.removeEventListener("click", handleCardClick);
 }
 
-/** Flip a card face-down. */
+/** Flip a card face-down. 
+ * 
+ * return card image to card back, replace event listener
+*/
 
 function unFlipCard(card) {
   card.classList.add("back");
@@ -72,18 +109,30 @@ function unFlipCard(card) {
 /** Handle clicking on a card: this could be first-card or second-card. */
 
 function handleCardClick(evt) {
+  //increment clicks counter
+  clicks ++;
+  updateCounters();
   let card = evt.target;
   //alert("You have clicked on " + card);
   flipCard(card);
-  //remove event listener so you can't click the same card twice
-  card.removeEventListener("click", handleCardClick);
+  
 
   //add this card to selected cards
   selectedCards.push(card);
 
   //check if we have a match if there are two cards in selectedCards array
   if(selectedCards.length === 2){
+
+    //make it so you can't click anymore cards!!!
+    let unclickedCards = document.getElementsByClassName("back");
+    for(let card of unclickedCards){
+      console.log("removing event listener from" + card.className)
+      card.removeEventListener("click", handleCardClick);
+    }
+    //check for match
     setTimeout(checkForMatch, FOUND_MATCH_WAIT_MSECS);
+
+    
   }
 
 }
@@ -98,14 +147,19 @@ function checkForMatch(){
   }
   //set selected cards back to 0
   selectedCards.length = 0;
+  //replace event listeners to unmatched cards
+  let unmatchedCards = document.getElementsByClassName("back");
+  for(let card of unmatchedCards){
+    card.addEventListener("click", handleCardClick);
+  }
+
 
 }
 
 function handleMatch(){
   //alert("This is a match!");
   score++;
-  let lblScore = document.getElementById('scoreDisplay');
-  lblScore.innerHTML = score;
+  updateCounters();
   checkForWin();
   //keep cards face up, and add 1 to score
 
@@ -121,7 +175,18 @@ function handleNotMatch(){
 }
 
 function checkForWin(){
-  if(score === NUM_PAIRS_TO_WIN){
+  console.log("Score: " + score);
+  console.log("Num of pairs to win: " +numPairsToWin);
+  if(score === numPairsToWin){
+    console.log("You win!")
     setTimeout(alert("You win!"), FOUND_MATCH_WAIT_MSECS);
   }
+}
+
+function updateCounters(){
+  let lblScore = document.getElementById('scoreDisplay');
+  lblScore.innerHTML = score;
+
+  let lblClicks = document.getElementById("clicksDisplay")
+  lblClicks.innerHTML = clicks;
 }
